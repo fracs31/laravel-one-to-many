e3e66925858b01e14d6b74ca222ca80efc809a5d
fracs31 <pist.fracs95@gmail.com>
2023-04-26 14:17:04 +0200

    ADD: Type's migration

--- database/migrations/2023_04_26_115741_create_types_table.php
+++ database/migrations/2023_04_26_115741_create_types_table.php
@@ -14,8 +14,10 @@ return new class extends Migration
     public function up()
     {
         Schema::create('types', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->id(); //id
+            $table->string("name")->unique(); //nome
+            $table->string("slug"); //slug
+            $table->timestamps(); //data di creazione e di aggiornamento
         });
     }
 
