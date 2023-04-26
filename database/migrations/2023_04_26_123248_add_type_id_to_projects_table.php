d83bfe0f1124558959e07f110d9072b60ea3f95e
fracs31 <pist.fracs95@gmail.com>
2023-04-26 14:38:05 +0200

    ADD: Type's id migration for the foreign key

---
+++ database/migrations/2023_04_26_123248_add_type_id_to_projects_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('projects', function (Blueprint $table) {
+            $table->foreignId("type_id")->constrained(); //chiave esterna
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('projects', function (Blueprint $table) {
+            $table->dropForeign("projects_type_id_foreign"); //cancello la chiave esterna
+            $table->dropColumn("type_id"); //cancello la colonna della chiave esterna
+        });
+    }
+};
